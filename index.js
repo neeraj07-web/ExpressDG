f699ebbb0096663f6ab86c7f71db1d3b24bf9ef2
neeraj07-web <neerajiiit07@gmail.com>
2025-06-03 17:42:44 +0530

    add

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 console.log("Hello from Section 21, Exercise 3!");
 console.log("This is a simple Express.js application to manage tea data.");
+console.log("You can add, retrieve, update, and delete tea items."); 
